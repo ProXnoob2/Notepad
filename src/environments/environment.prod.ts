4665912a1dc7853882a6b119d3fc217a8e4edf9e
ProXnoob2 <therealgameradeeb@gmail.com>
2022-10-24 19:29:31 +0530

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDwaytqbTzS2NKdFo9gWlPtjhEnBS-6wHM",
+    authDomain: "addyournotes.firebaseapp.com",
+    projectId: "addyournotes",
+    storageBucket: "addyournotes.appspot.com",
+    messagingSenderId: "713097580534",
+    appId: "1:713097580534:web:a0b2510b5aed08d38301d7",
+    measurementId: "G-FTYW6Z55V5"
+  }
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDwaytqbTzS2NKdFo9gWlPtjhEnBS-6wHM",
+    authDomain: "addyournotes.firebaseapp.com",
+    projectId: "addyournotes",
+    storageBucket: "addyournotes.appspot.com",
+    messagingSenderId: "713097580534",
+    appId: "1:713097580534:web:a0b2510b5aed08d38301d7",
+    measurementId: "G-FTYW6Z55V5"
+  }
 };
 
 /*
